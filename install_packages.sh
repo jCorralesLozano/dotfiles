7b77c32ed050fb049878ab1416bffa5a2076003c
Jesse Corrales-Lozano <jesselozano17@gmail.com>
2020-08-01 18:05:41 -0400

    Updated packages to be installed on newer kali systems

--- install_packages.sh
+++ install_packages.sh
@@ -1,8 +1,21 @@
 # make sure everything is up to date
-sudo apt-get update && sudo apt-get upgrade
+# sudo apt-get update && sudo apt-get upgrade
 
 # install guest additions (VM only)
-sudo apt install -y virtualbox-guest-x11
+# sudo apt install -y virtualbox-guest-x11
 
-# clipboard program
-sudo apt install xclip
+# sudo apt install xclip # clipboard program
+sudo apt install powercat
+sudo apt install seclists
+sudo apt install masscan
+sudo apt install exploitdb
+sudo apt install mingw-w64
+sudo apt install pure-ftpd # dont forget the setup script
+sudo apt install atftp # non interactive ftp for older OS's 
+sudo apt install shellter # av evasion program
+# sudo apt install wine >> learn how to setup first 
+sudo apt install crowbar # network authentication cracking tool
+sudo apt install rinetd # port forwading tool
+sudo apt install httptunnel # encapsulate traffic in http tunnel, look for setup script
+sudo apt install kerberoast 
+sudo apt install metasploit-framework
