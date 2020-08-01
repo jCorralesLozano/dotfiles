007ae6ea237da17880878d54fbcede1ad11a859d
Jesse <jesselozano17@gmail.com>
2020-08-01 15:52:52 -0400

    First commit of empty dotfiles

---
+++ install.sh
@@ -0,0 +1,25 @@
+# base directory
+BASEDIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+
+# verify the existence of specific dotfiles
+for file in ./.{bash_profile,bashrc,tmux.conf,vimrc}; do
+	if [ -r "$file" ] && [ -f "$file" ]; then
+		# trim the dot and fslash for the $file variable
+		file=`echo $file | sed 's:^\./*::g'`
+
+		# create symlink in users home directory
+		ln -s ${BASEDIR}/${file} ~/${file}
+		echo "[*] symlink for ${file} has been placed in home directory"
+	fi;
+done;
+
+# install tmux plugin manager
+# git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+
+# install tmux plugins
+# ~/.tmux/plugins/tpm/bin/install_plugins
+echo '[*] tmux plugin manager installed'
+
+# update tmux plugins
+# ~/.tmux/plugins/tpm/bin/update_plugins all
+echo '[*] tmux plugins updated'
